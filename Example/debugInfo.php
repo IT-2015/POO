b4915ec03449d104b0924553cdec9a0da90fbd0e
sferey <s.ferey@cleo-consulting.fr>
2015-10-08 10:16:58 +0200

    Create debugInfo.php

---
+++ Example/debugInfo.php
@@ -0,0 +1,16 @@
+<?php
+class C {
+    private $prop;
+
+    public function __construct($val) {
+        $this->prop = $val;
+    }
+
+    public function __debugInfo() {
+        return [
+            'propSquared' => $this->prop ** 2,
+        ];
+    }
+}
+
+var_dump(new C(42));
