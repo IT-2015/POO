cb09eafe08ead24f84ae97f40d5406c18c9afe98
sferey <s.ferey@cleo-consulting.fr>
2015-10-21 15:54:04 +0200

    Create Contact.php

---
+++ Example/TP/Contact.php
@@ -0,0 +1,25 @@
+<?php
+class Form_Contact extends Form {
+	
+	public function init(){
+
+		$inputText = new Form_Element([
+			'name' => 'test',
+			'type' => 'text',
+			'class'=> 'form-input',
+			'placeholder' => 'Test'
+		]);
+
+		$inputSubmit = new Form_Element([
+			'name' => 'send',
+			'type' => 'submit',
+			'class'=> 'form-submit'
+		]);
+
+		$this->setMethod('POST');
+		$this->setAction('');
+		$this->addElement($inputText);
+		$this->addElement($inputSubmit);
+
+	}
+}
